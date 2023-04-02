e975270435dff960e47d0bbeeac9b9a0888f7523
ZLM <2479326918@qq.com>
2023-04-02 22:43:15 +0800

    button组件封装，自定义主题色

--- component-package/vue.config.js
+++ component-package/vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+// })
+
+module.exports = {
+  lintOnSave: false,
+  //使用button的主题色配置文件
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: '@import "./src/styles/scssconfig.scss";'
+      }
+    },
+  }
+}
